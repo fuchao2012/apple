38b2f9b5af45957d9901562788ca78d405f53273
fuchao <zhengfuchao2008@126.com>
2014-12-13 21:19:02 +0800

    change the image color

--- js/apple-fuchao.js
+++ js/apple-fuchao.js
@@ -19,49 +19,25 @@ $(document).ready(function () {
 		}
 	});
 });
+//change image color
+function change_silicone(color){
+    var select = document.getElementById('gallery-silicone-iphone-6-cases').childNodes;
+    var target = select[1].childNodes[1];
+    target.className = 'silicone-iphone-6-'+color.trim()+'-case-image';
+}
+function change_leather(color){
+    var select = document.getElementById('gallery-leather-iphone-6-cases').childNodes;
+    var target = select[1].childNodes[1];
+    target.className = 'leather-iphone-6-'+color.trim()+'-case-image';
+}
+function change_silicone_plus(color){
+    var select = document.getElementById('gallery-silicone-iphone-6-plus-cases').childNodes;
+    var target = select[1].childNodes[1];
+    target.className = 'silicone-iphone-6-plus-'+color.trim()+'-case-image';
+}
+function change_leather_plus(color){
+    var select = document.getElementById('gallery-leather-iphone-6-plus-cases').childNodes;
+    var target = select[1].childNodes[1];
+    target.className = 'leather-iphone-6-plus-'+color.trim()+'-case-image';
+}
 
-
-//slogn
-//var mainslider;
-//
-//$(document).ready(function(){
-//    var options = {
-//        slides: '.slide', // The name of a slide in the slidesContainer
-//        swipe: true,    // Add possibility to Swipe > note that you have to include touchSwipe for this
-//        slideTracker: true, // Add a UL with list items to track the current slide
-//        slideTrackerID: 'slideposition', // The name of the UL that tracks the slides
-//        slideOnInterval: false, // Slide on interval
-//        interval: 9000, // Interval to slide on if slideOnInterval is enabled
-//        animateDuration: 1000, // Duration of an animation
-//        animationEasing: 'ease', // Accepts: linear ease in out in-out snap easeOutCubic easeInOutCubic easeInCirc easeOutCirc easeInOutCirc easeInExpo easeOutExpo easeInOutExpo easeInQuad easeOutQuad easeInOutQuad easeInQuart easeOutQuart easeInOutQuart easeInQuint easeOutQuint easeInOutQuint easeInSine easeOutSine easeInOutSine easeInBack easeOutBack easeInOutBack
-//        pauseOnHover: false // Pause when user hovers the slide container
-//    };
-//
-//    $(".slider").simpleSlider(options);
-//    mainslider = $(".slider").data("simpleslider");
-//    /* yes, that's all! */
-//
-//    $(".slider").on("beforeSliding", function(event){
-//        var prevSlide = event.prevSlide;
-//        var newSlide = event.newSlide;
-//        $(".slider .slide[data-index='"+prevSlide+"'] .slidecontent").fadeOut();
-//        $(".slider .slide[data-index='"+newSlide+"'] .slidecontent").hide();
-//    });
-//
-//    $(".slider").on("afterSliding", function(event){
-//        var prevSlide = event.prevSlide;
-//        var newSlide = event.newSlide;
-//        $(".slider .slide[data-index='"+newSlide+"'] .slidecontent").fadeIn();
-//    });
-//
-//    $(".slide#first").backstretch("images/bg1.jpg");
-//    $(".slide#sec").backstretch("images/bg2.jpg");
-//    $(".slide#thirth").backstretch("images/bg4.jpg");
-//    $(".slide#fourth").backstretch("images/bg5.jpg");
-//
-//    $('.slide .backstretch img').on('dragstart', function(event) { event.preventDefault(); });
-//
-//    $(".slidecontent").each(function(){
-//        $(this).css('margin-top', -$(this).height()/2);
-//    });
-//});
